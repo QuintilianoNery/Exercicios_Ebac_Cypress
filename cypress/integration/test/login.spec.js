e61805d78dd866fb3ca693836cff9d2284b93f9e
Quintiliano Paquini Nery <qpnery@paschoalotto.com.br>
2022-01-03 18:42:04 -0300

    Conclusão Aula 2 M11

--- cypress/integration/test/login.spec.js
+++ cypress/integration/test/login.spec.js
@@ -2,7 +2,7 @@
 
 //Bloco de funcionalidades 
 context('Funcionalidade Login', () => {
-    it.only('Deve fazer login com sucessso', () => {
+    it('Deve fazer login com sucessso', () => {
         cy.visit('/')
         cy.get('i[class="icon-user-unfollow icons"]').click()
         cy.get('input[id="username"]').type(Cypress.env('usuarioEbac'));
@@ -11,19 +11,36 @@ context('Funcionalidade Login', () => {
         cy.get('input[name="login"]').click();
         cy.url()
             .should('be.equal', 'http://lojaebac.ebaconline.art.br/minha-conta/')
-       cy.get('span[class="hidden-xs"]')
-            .should('have.text', 'Welcome aluno_ebac !') 
+        cy.get('span[class="hidden-xs"]')
+            .should('have.text', 'Welcome aluno_ebac !')
     });
-    it('Deve exibir uma mensagem de erro ao insserir um usuário e senha inválido', () => {
+    it('Deve exibir uma mensagem de erro ao insserir um usuário inválido', () => {
         cy.visit('/')
         cy.get('i[class="icon-user-unfollow icons"]').click()
-        cy.get('input[id="username"]').type('email@invalido.com.br');
+        cy.get('input[id="username"]').type('a@a.com');
+        cy.get('input[id="password"]').type('teste@teste.com');
+        cy.get('input[id="rememberme"]').click();
+        cy.get('input[name="login"]').click();
+        //O alerta deve ser exibido
+        cy.get('div[class="woocommerce-notices-wrapper"]')
+            .should('be.visible')
+        cy.get('.woocommerce-error')
+            .should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+    });
+
+    it('Deve exibir uma mensagem de erro ao insserir senha inválida', () => {
+        cy.visit('/')
+        cy.get('i[class="icon-user-unfollow icons"]').click()
+        cy.get('input[id="username"]').type('aluno_ebac@teste.com');
         cy.get('input[id="password"]').type('senhaInvalida');
         cy.get('input[id="rememberme"]').click();
         cy.get('input[name="login"]').click();
         //O alerta deve ser exibido
         cy.get('div[class="woocommerce-notices-wrapper"]')
             .should('be.visible')
+        cy.get('.woocommerce-error')
+            .should('contain', 'A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+
     });
 
 });
