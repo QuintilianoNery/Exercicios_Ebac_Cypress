630f31f6309c6aeefc5f1be213095329878d1181
Quintiliano <qpnery@paschoalotto.com.br>
2022-07-24 17:21:17 -0300

    Configuração Index.js para utilizar o allure report

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,4 +22,4 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = (on, config) => {
     allureWriter(on, config);
     return config;
-};
+}
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,6 +18,3 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-import '@shelex/cypress-allure-plugin';
-require('@shelex/cypress-allure-plugin');
