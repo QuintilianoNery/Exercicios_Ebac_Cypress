f440d1b5fff7003628fc9cb520026d0f405086fb
Quintiliano Paquini Nery <quintiliano.nery@take.nery>
2022-08-29 09:31:47 -0300

    Update confg runner

--- runner.js
+++ runner.js
@@ -3,10 +3,15 @@ const tesults = require('cypress-tesults-reporter');
 
 cypress.run({
   // specs to run here
+  browser: 'chrome',
+  config: {
+    video: true
+  }
 })
 .then((results) => {
+  console.log(results)
   const args = {
-    target: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjA2MWRkNGYxLTcxOWYtNGQ4Ni05ZjBlLWEwMWY2NmVkYjQzNy0xNjYwMjc4MzE4ODcxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiMjdlNGZiY2MtYWNkNy00MTMyLWI4MmQtNzMwYWFlOTA1ZTdiIiwidHlwZSI6InQifQ.pseS0OLYvY7xbPF6WHY2tBgxca8q5C7JWpuX3gZam-k',
+    target: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjA2MWRkNGYxLTcxOWYtNGQ4Ni05ZjBlLWEwMWY2NmVkYjQzNy0xNjYwMjc4MzE4ODcxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiY2RmNzQ3NmItMTc3NS00ZWU5LWFhZDYtNzY5YmVkZjk3MzM5IiwidHlwZSI6InQifQ.eitxXB-5tFJJEzFlGkzVOs_25FABMUGfIYrJIZ6ZaKw',
   }
   tesults.results(results, args);
 })
