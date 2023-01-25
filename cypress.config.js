0c2060b369bcda9b79f0eec8755bb7bcbd98a5c7
Quintiliano Paquini Nery <quintiliano.nery@take.nery>
2023-01-25 18:22:53 -0300

    modificações

--- cypress.config.js
+++ cypress.config.js
@@ -2,17 +2,6 @@ const { defineConfig } = require('cypress')
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  websecurity: false,
-  video: true,
-  viewportWidth: 1600,
-  viewportHeight: 900,
-  chromeWebSecurity: false,
-  projectId: 'fpi71k',
-  titulo: "usuarioEBAC",
-  usuarioEbac: "aluno_ebac@teste.com",
-  senhaEbac: "teste@teste.com",
-
-
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -22,6 +11,5 @@ module.exports = defineConfig({
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
-
 
 })
