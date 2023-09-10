77f472a44051cd3e3dc9897eca6b7b1124085bdb
Quintiliano <quintiliano1995@gmail.com>
2023-09-10 17:16:03 -0300

    Atualização versão node na pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -14,8 +14,6 @@ module.exports = defineConfig({
 
 
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       allureWriter(on, config)
     },
